1a83831972673729447a0627c256f221ee6c5492
AngelSeth <sethmoosepic@gmail.com>
2024-03-11 13:06:05 -0700

    Initial commit

---
+++ src/main/java/mythcore/levelingcore/SoloLeveling.java
@@ -0,0 +1,19 @@
+package mythcore.levelingcore;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SoloLeveling implements ModInitializer {
+
+	public static final String MOD_ID = "sololeveling";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
